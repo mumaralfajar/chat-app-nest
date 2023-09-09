5b07e976f90ff38771aaa138b2a8428644eac026
Budi <budimanfajarf@gmail.com>
2023-09-09 12:21:41 +0700

    Update logging

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
@@ -8,6 +8,6 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port');
   await app.listen(port);
-  Logger.log(`Application is running on: ${port}`, 'App');
+  Logger.log(`Nest application is running on port: ${port}`, NestApplication.name);
 }
 bootstrap();
