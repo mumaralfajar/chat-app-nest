68e9c93c06d02308e6e8a3600ea3753b6a0213c8
Budi <budimanfajarf@gmail.com>
2023-09-12 14:04:37 +0700

    Simplify mongodb config

--- src/config/mongodb.config.ts
+++ src/config/mongodb.config.ts
@@ -1,19 +1,11 @@
 import { registerAs } from '@nestjs/config';
 
 const alias = 'mongodb';
-const baseConfig = {
-  host: process.env.MONGODB_HOST || '127.0.0.1',
-  port: parseInt(process.env.MONGODB_PORT, 10) || 27017,
-  user: process.env.MONGODB_USER,
-  password: process.env.MONGODB_PASSWORD,
-};
 
 export const mongodbConfig = registerAs(alias, () => ({
-  ...baseConfig,
-  database: process.env.MONGODB_DATABASE || 'chatapp',
+  uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/chatapp',
 }));
 
 export const mongodbTestConfig = registerAs(alias, () => ({
-  ...baseConfig,
-  database: process.env.MONGODB_DATABASE_TEST || 'chatapp_test',
+  uri: process.env.MONGODB_URI_TEST || 'mongodb://localhost:27017/chatapp_test',
 }));
--- src/config/mongoose.config-service.ts
+++ src/config/mongoose.config-service.ts
@@ -6,34 +6,17 @@ import { mongodbConfig } from './mongodb.config';
 @Injectable()
 export class MongooseConfigService implements MongooseOptionsFactory {
   private readonly logger = new Logger(MongooseConfigService.name);
-  protected uri: string;
 
   constructor(
     @Inject(mongodbConfig.KEY)
     private readonly mongoConfig: ConfigType<typeof mongodbConfig>,
-  ) {
-    const mongodb = 'mongodb://';
-    const host = this.mongoConfig.host;
-    const port = this.mongoConfig.port;
-    const user = this.mongoConfig.user;
-    const password = this.mongoConfig.password;
-    const database = this.mongoConfig.database;
-
-    // console.log({ mongodb, host, port, user, password, database });
-
-    if (user && password) {
-      this.uri = `${mongodb}${user}:${password}@${host}:${port}/${database}`;
-      return;
-    }
-
-    this.uri = `${mongodb}${host}:${port}/${database}`;
-  }
+  ) {}
 
   createMongooseOptions(): MongooseModuleOptions {
-    this.logger.log(`uri: ${this.uri}`);
+    this.logger.log(`uri: ${this.mongoConfig.uri}`);
 
     return {
-      uri: this.uri,
+      uri: this.mongoConfig.uri,
     };
   }
 }
